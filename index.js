8edcb1ec5b915a283bfc417d91f7f180ff3aba36
blue linden <dev@bluelinden.art>
2023-04-19 18:45:39 -0400

    simplify code

--- index.js
+++ index.js
@@ -8,29 +8,25 @@
  */
 
 function contiguousRange(array, atIndex) {
-  
-  var workArray = array.slice(); // slice the array so that we're working with our own copy
-
   var startIndex, endIndex
 
-  for (var i = atIndex, len = workArray.length; i < len; i++) { // iterate forward from the starting point to get end index
-    if(workArray[i] == workArray[atIndex]) { // if the value of this element and the original element match, this is part of a contiguous run of elements.
-      endIndex = i // just keep on setting the end index higher until it no longer matches
+  for (var i = atIndex, len = array.length; i < len; i++) { // iterate forward from the starting point to get end index
+    if(array[i] == array[atIndex]) { // if the value of this element and the original element match, this is part of a contiguous run of elements.
+      endIndex = i; // just keep on setting the end index higher until it no longer matches
     } else {
-      i = workArray.length // prematurely break from the loop
+      break;
     }
   }
 
-  for(var i = atIndex, len = workArray.length; i > 0; i--) { // iterate backward from the starting point to get start index
-    if(workArray[i] == workArray[atIndex]) { // if the value of this element and the original element match, this is part of a contiguous run of elements.
+  for(var i = atIndex, len = array.length; i > 0; i--) { // iterate backward from the starting point to get start index
+    if(array[i] == array[atIndex]) { // if the value of this element and the original element match, this is part of a contiguous run of elements.
       startIndex = i // just keep on setting the start index lower until it no longer matches
     } else {
-      i = 0 // prematurely break from the loop
+      break;
     }
   }
 
   var indices = {}
-  indices.count = (endIndex + 1) - startIndex // get count of elements in the run
   indices.start = startIndex
   indices.end = endIndex 
   return indices;
